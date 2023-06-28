f38aff099b860bd370a89145d68a7779dbbf8264
xunjianxiang <xunjianxiang@gmail.com>
2023-06-29 02:58:22 +0800

    feat: set redirect url form index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import { Redirect } from '@docusaurus/router';
 import React from 'react';
 
 export default function IndexPage() {
-  return (<div>Docs Home</div>)
+  // return (<div>Docs Home</div>)
+  return <Redirect to={'/docs/dozer'} />
 }
