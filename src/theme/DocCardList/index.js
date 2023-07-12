db7d6c5633a440f2d0d756c054cb48ad2bc96b6f
jianxiangxun <xunjianxiang@aliyun.com>
2023-07-13 00:10:37 +0800

    feat: eject DocCardList

---
+++ src/theme/DocCardList/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import clsx from 'clsx';
+import {
+  useCurrentSidebarCategory,
+  filterDocCardListItems,
+} from '@docusaurus/theme-common';
+import DocCard from '@theme/DocCard';
+function DocCardListForCurrentSidebarCategory({className}) {
+  const category = useCurrentSidebarCategory();
+  return <DocCardList items={category.items} className={className} />;
+}
+export default function DocCardList(props) {
+  const {items, className} = props;
+  if (!items) {
+    return <DocCardListForCurrentSidebarCategory {...props} />;
+  }
+  const filteredItems = filterDocCardListItems(items);
+  console.log('DocCardList', filteredItems);
+  return (
+    <section className={clsx('row', className)}>
+      {filteredItems.filter(item => ['category', 'link'].includes(item.type)).map((item, index) => (
+        <article key={index} className="col col--6 margin-bottom--lg">
+          <DocCard item={item} />
+        </article>
+      ))}
+    </section>
+  );
+}
